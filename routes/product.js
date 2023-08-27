900c79a60cbea9d2a9ab10aa63da1c579515e8f6
Hemant2335 <knrt73373@gmail.com>
2023-08-27 11:47:45 +0530

    updated_routes_and_models

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,7 @@
 const product = require("../models/Products");
 const express = require("express");
+const Category = require("../models/Category");
+const Genre = require("../models/Genre");
 
 const router = express.Router();
 
@@ -8,6 +10,28 @@ router.get("/products", async(req, res) => {
     try {
         const products = await product.find();
         res.json(products);
+    } catch (error) {
+        console.log("Some Error Occuered");
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.get("/category", async(req, res) => {
+
+    try {
+        const category = await Category.find();
+        res.json(category);
+    } catch (error) {
+        console.log("Some Error Occuered");
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.get("/genre", async(req, res) => {
+
+    try {
+        const genre = await Genre.find();
+        res.json(genre);
     } catch (error) {
         console.log("Some Error Occuered");
         res.status(500).json({ error: "Internal Server Error" });
