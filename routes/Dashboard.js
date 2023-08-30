48f704578344cb7e454d4cd0a408cc94a369a99f
Hemant2335 <knrt73373@gmail.com>
2023-08-30 11:00:25 +0530

    minor_changes

--- routes/Dashboard.js
+++ routes/Dashboard.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const fetchuser = require("../middlewares/fetchuser");
+const cloudinary = require('cloudinary');
 
 const router = express.Router();
 
+cloudinary.v2.config({
+  cloud_name: 'dt7uzawmf',
+  api_key: '919963862632711',
+  api_secret: 'agNtvWOz5AZRn70xD1F8O_la_0o',
+  secure: true,
+});
 
-router.post("/upload/:uid", fetchuser ,async (req, res) => {
-    try {
-        console.log("I am upload")
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+router.post("/upload", fetchuser ,async (req, res) => {
+// Path to the image you want to upload
+const imagePath = "C:\Users\knrt7\Downloads\poster.jpeg";
+// Upload the image to Cloudinary
+cloudinary.uploader.upload(imagePath, { folder: 'uploads' }, (error, result) => {
+  if (error) {
+    console.error('Upload error:', error);
+    res.status(400).send("Some Error Occured")
+  } else {
+    res.status(200).send({status : 'Upload successful', result : result})
+    console.log('Upload successful:', result);
+    // You can access the uploaded image URL using result.url
+  }
+});
+
 });
 
 module.exports = router;
