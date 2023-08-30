e66d2ab83d498bd876a10effdca7d1bcef155a0f
Hemant2335 <knrt73373@gmail.com>
2023-08-31 02:40:18 +0530

    upload_route_done

--- routes/Dashboard.js
+++ routes/Dashboard.js
@@ -15,29 +15,32 @@ cloudinary.v2.config({
 const storage = multer.memoryStorage(); // Stores file in memory as buffer
 const upload = multer({ storage: storage });
 
-router.post("/upload/:uid", fetchuser, upload.single('image') ,async (req, res) => {
+router.post("/upload/:uid", fetchuser, upload.single('image'), async (req, res) => {
   try {
     // Access the uploaded file from req.file
     const fileBuffer = req.file.buffer;
-    
-    const result = await cloudinary.uploader.upload_stream(
-      { folder: 'uploads' },
-      (error, result) => {
-        if (error) {
-          console.error('Upload error:', error);
-          res.status(400).send("Some Error Occurred");
-        } else {
-          console.log('Upload successful:', result);
-          res.status(200).send({ status: 'Upload successful', result: result });
-        }
+    let Check = false;
+
+    // Convert the buffer to a readable stream
+    const readableStream = cloudinary.v2.uploader.upload_stream({ folder: 'uploads' }, (error, result) => {
+      if (error) {
+        console.error('Upload error:', error);
+        res.status(400).send({ Check: Check, error: "Some Error Occurred" });
+      } else {
+        Check = true;
+        const imageUrl = result.secure_url;
+        res.status(200).send({ Check: Check, status: 'Upload successful', imageUrl: imageUrl });
       }
-    ).end(fileBuffer);
+    });
+
+    // Write the buffer to the readable stream
+    readableStream.write(fileBuffer);
+    readableStream.end();
 
   } catch (error) {
     console.error('Upload error:', error);
     res.status(400).send("Some Error Occurred");
   }
-  
 });
 
 module.exports = router;
